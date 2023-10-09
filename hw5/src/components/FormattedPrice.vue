24cd59f16ff36bfd93602947a1a759d87533adfd
Grisha <pin.pinigin@mail.ru>
2023-10-09 08:13:10 +0300

    hw5

---
+++ hw5/src/components/FormattedPrice.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        {{ foormattedPrice }}
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            price: Number
+        },
+        
+        name: 'formattedPrice',
+        data(){
+            return{
+                currencyFormat: '$'
+            }
+        },
+        computed:{
+            foormattedPrice() {
+                return(this.currencyFormat + this.price)
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
